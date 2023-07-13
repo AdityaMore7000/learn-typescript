af8f1ac5347efc2ad852099aa50cde404ba718a6
AdityaMore7000 <adi.more7000@gmail.com>
2023-07-13 15:03:35 +0530

    react remains

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,26 @@
 "use strict";
 // Basic types
 var a = 3;
-console.log('A:', a);
-let company = 'Apple';
+// console.log('A:', a);
+let company = "Apple";
 let isPublished = true;
 let age = 30;
 let x = true;
 x = 3;
 //Arrays:
 let ids = [1, 2, 3, 4, 5];
-let arr = [1, 'a', true];
+let arr = [1, "a", true];
 //Tuple
-let person = [1, 'Name', true,];
+let person = [1, "Name", true];
 //any can use any type but tuple lets us specify types also order is imp, num,string,bool is ok.string,num,bool is not.
 //Tuple array:
 let employee;
 employee = [
-    ['Aditya', 12],
+    ["Aditya", 12],
     ["Mom", 13],
-    ["Dad", 14]
+    ["Dad", 14],
 ];
-console.log(employee);
+// console.log(employee);
 //unions
 let id = 22;
 id = "12";
@@ -33,4 +33,66 @@ var Direction1;
     Direction1[Direction1["Right"] = 21] = "Right";
     Direction1[Direction1["Left"] = 22] = "Left";
 })(Direction1 || (Direction1 = {}));
-console.log(Direction1.Right);
+var Direction2;
+(function (Direction2) {
+    Direction2["Top"] = "Top";
+    Direction2["Bottom"] = "Bottom";
+    Direction2["Right"] = "Right";
+    Direction2["Left"] = "Left";
+})(Direction2 || (Direction2 = {}));
+// console.log(Direction1.Right)
+// console.log(Direction2.Right)
+// Objects:
+let user = {
+    id: 55,
+    name: "Saarthak",
+};
+let user2 = {
+    id: 52,
+    name: "Aditya",
+};
+//Type assertion:
+let cid = 3;
+let customerID = cid;
+// OR
+customerID = cid;
+// Functions:
+function addNum(x, y) {
+    return x + y;
+}
+const ans = addNum(1, 2);
+// console.log(ans)
+// If functions do not return anything, you can use void as there data type
+function printDouble(x) {
+    // console.log(x * 2);
+}
+printDouble(21);
+const ui = {
+    id: 12,
+    age: 13,
+    name: "Hello",
+};
+const add = (x, y) => (x + y);
+console.log(add(12, 13));
+//classes:
+// properties are public by default,you can make them private or protected
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        console.log(id + " " + name);
+    }
+}
+const Wil = new Person(1987, 'wil');
+const mike = new Person(2023, 'mike');
+class People extends Person {
+    constructor(id, name, age) {
+        super(id, name);
+        this.age = age;
+        console.log(this.id);
+    }
+}
+const Floof = new People(41, "Lauka", 20);
+console.log(Floof.age);
+console.log(Floof.name);
+// Generic functions 
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,145 @@
 // Basic types
 var a: number = 3;
-console.log('A:', a);
-let company: string = 'Apple'
+// console.log('A:', a);
+let company: string = "Apple";
 let isPublished: boolean = true;
 let age: number = 30;
 let x: any = true;
 x = 3;
 
 //Arrays:
-let ids: number[] = [1, 2, 3, 4, 5]
-let arr: any[] = [1, 'a', true]
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, "a", true];
 
 //Tuple
-let person: [number, string, boolean] = [1, 'Name', true,]
+let person: [number, string, boolean] = [1, "Name", true];
 
 //any can use any type but tuple lets us specify types also order is imp, num,string,bool is ok.string,num,bool is not.
 
 //Tuple array:
-let employee: [string, number][]
+let employee: [string, number][];
 
 employee = [
-    ['Aditya', 12],
+    ["Aditya", 12],
     ["Mom", 13],
-    ["Dad", 14]
-]
+    ["Dad", 14],
+];
 
-console.log(employee);
+// console.log(employee);
 //unions
-let id:string|number|boolean = 22;
+let id: string | number | boolean = 22;
 
-id="12"
-id=true
+id = "12";
+id = true;
 
 //enums:named constants
 
-enum Direction1{
-    Top =10,
-    Bottom=20, 
+enum Direction1 {
+    Top = 10,
+    Bottom = 20,
     Right,
-    Left
+    Left,
 }
-console.log(Direction1.Right)
+enum Direction2 {
+    Top = "Top",
+    Bottom = "Bottom",
+    Right = "Right",
+    Left = "Left",
+}
+// console.log(Direction1.Right)
+// console.log(Direction2.Right)
+
+// Objects:
+let user: {
+    id: number;
+    name: string;
+} = {
+    id: 55,
+    name: "Saarthak",
+};
+
+// OR:
+type User = {
+    id: number;
+    name: string;
+};
+let user2: User = {
+    id: 52,
+    name: "Aditya",
+};
+
+//Type assertion:
+let cid: any = 3;
+let customerID = <number>cid;
+
+// OR
+
+customerID = cid as number;
+
+// Functions:
+function addNum(x: number, y: number): number {
+    return x + y;
+}
+
+const ans: number = addNum(1, 2);
+// console.log(ans)
+
+// If functions do not return anything, you can use void as there data type
+function printDouble(x: number): void {
+    // console.log(x * 2);
+}
+printDouble(21);
+
+// Interface is like a type only but,
+// type can be used with primitives as well as unions but interface cannot.
+
+interface UI {
+    id: number;
+    readonly name: string; // this cannot be overwritten now
+    age?:number; // ? denotes optional attributes.
+}
+const ui: UI = {
+    id: 12,
+    age:13,
+    name: "Hello",
+};
+
+// interface with func
+interface mathFunc{ 
+    (x:number,y:number):number
+}
+
+const add:mathFunc = (x,y)=>(x+y)
+
+console.log(add(12,13))
+
+
+//classes:
+// properties are public by default,you can make them private or protected
+class Person {
+    protected id:number;
+    name:string;
+    constructor(id:number,name:string){
+        this.id = id;
+        this.name = name;
+        console.log(id+" "+name)
+    }
+}
+const Wil = new Person(1987,'wil');
+const mike = new Person(2023,'mike');
+
+class People extends Person{
+    age:number;
+    constructor(id:number, name:string, age:number){
+        super(id,name);
+        this.age = age
+        console.log(this.id);
+        
+    }
+}
+
+const Floof = new People(41,"Lauka",20);
+console.log(Floof.age);
+console.log(Floof.name);
+
+// Generic functions 
